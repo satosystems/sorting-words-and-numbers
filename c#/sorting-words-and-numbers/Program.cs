e32b517f16566fad3f4f2af51f710b622a411d8d
Satoshi Ogata <satosystems@gmail.com>
2022-02-16 22:58:11 +0900

    Implement the process of C#
    処理を実装。
    プログラム終了は割り込みで行う。

--- c#/sorting-words-and-numbers/Program.cs
+++ c#/sorting-words-and-numbers/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace sortingwordsandnumbers
 {
     class MainClass
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                var line = Console.ReadLine();
+                var splitted = line.Split(' ');
+                var sorted = splitted.Aggregate((new List<string>(), new List<string>()), (acc, cur) =>
+                {
+                    if (cur.All(Char.IsNumber))
+                    {
+                        acc.Item1.Add(cur);
+                    }
+                    else
+                    {
+                        acc.Item2.Add(cur);
+                    }
+                    return acc;
+                });
+                sorted.Item1.ForEach(x => Console.Write("{0},", x));
+                Console.WriteLine();
+                sorted.Item2.ForEach(x => Console.Write("{0},", x));
+                Console.WriteLine();
+            }
         }
     }
 }
