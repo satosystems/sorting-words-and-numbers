b5e8d8c82a379da29df4e02c6560710d18497a1f
Satoshi Ogata <satosystems@gmail.com>
2022-02-16 19:55:33 +0900

    Implement the process of Haskell
    処理を実装。
    プログラム終了は割り込みで行う。

--- haskell/sorting-words-and-numbers/src/Lib.hs
+++ haskell/sorting-words-and-numbers/src/Lib.hs
@@ -1,6 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
   ( someFunc
   ) where
 
+import           Data.Char                      ( isNumber )
+import           Data.Foldable                  ( foldl' )
+import qualified Data.Text                     as T
+
+sort :: T.Text -> ([T.Text], [T.Text])
+sort input =
+  foldl'
+      (\acc cur -> if T.all isNumber cur
+        then (fst acc, snd acc ++ [cur])
+        else (fst acc ++ [cur], snd acc)
+      )
+      ([], [])
+    $ T.splitOn " " input
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = getLine >>= print . sort . T.pack >> someFunc
