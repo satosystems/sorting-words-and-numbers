0e0b861af29fc806b56b486aa66e4cf7a943f8d1
Satoshi Ogata <satosystems@gmail.com>
2022-02-17 16:15:02 +0900

    Implement the process of Ruby
    処理を実装。
    プログラム終了は割り込みで行う。

--- ruby/sorting-words-and-numbers/main.rb
+++ ruby/sorting-words-and-numbers/main.rb
@@ -1,1 +1,16 @@
+# frozen_string_literal: true
 
+loop do
+  sorted = [[], []]
+  line = gets
+  next if line.nil?
+
+  line.split(' ').each do |word|
+    if /\d+/.match(word)
+      sorted[1].append(word)
+    else
+      sorted[0].append(word)
+    end
+  end
+  print(sorted, "\n")
+end
