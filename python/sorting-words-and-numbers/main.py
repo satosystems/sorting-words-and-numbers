f43d3709f5e25101d06bc6ab1437519e0d72640d
Satoshi Ogata <satosystems@gmail.com>
2022-02-17 15:46:39 +0900

    Implement the process of Python
    処理を実装。
    プログラム終了は割り込みで行う。

--- python/sorting-words-and-numbers/main.py
+++ python/sorting-words-and-numbers/main.py
@@ -1,6 +1,13 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+def sort():
+    sorted_words = ([], [])
+    for word in input().split():
+        if word.isnumeric():
+            sorted_words[1].append(word)
+        else:
+            sorted_words[0].append(word)
+    print(sorted_words)
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    while True:
+        sort()
