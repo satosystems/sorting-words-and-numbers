9fc213dd3f5d2c774bd0a80c1746d6ac163bf308
Satoshi Ogata <satosystems@gmail.com>
2022-02-17 16:47:10 +0900

    Implement the process of C++
    処理を実装。
    プログラム終了は割り込みで行う。

--- c++/sorting-words-and-numbers/main.cpp
+++ c++/sorting-words-and-numbers/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <sstream>
+#include <vector>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "EndlessLoop"
+    while (true) {
+        std::vector<std::string> str;
+        std::vector<std::string> num;
+        std::string word;
+        std::string line;
+        std::getline(std::cin, line);
+        std::stringstream ss(line);
+        while (std::getline(ss, word, ' ')) {
+            if (word.find_first_not_of("0123456789") == std::string::npos) {
+                num.push_back(word);
+            } else {
+                str.push_back(word);
+            }
+        }
+
+        for (const std::string& s : str) {
+            std::cout << s << " ";
+        }
+        std::cout << std::endl;
+        for (const std::string& s : num) {
+            std::cout << s << " ";
+        }
+        std::cout << std::endl;
+    }
+#pragma clang diagnostic pop
     return 0;
 }
